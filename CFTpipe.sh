9e86fb69640996f5ca31da4abf5351186914d4a5
mshansen <hansenms@gwu.edu>
2014-02-19 17:34:05 -0500

    Update CFTpipe.sh

--- CFTpipe.sh
+++ CFTpipe.sh
@@ -1,33 +1,57 @@
 #!/bin/bash
 
-ARRAY=(`find ~/transcriptomes/ -name '*.fastq'`)                                        # places paths to files ending in .fastq into an array
-ARRAY1=(`find ~/transcriptomes/ -name '*.fastq' -exec basename {} \;`)                  # places files ending in .fastq into an array
+home=~/transcriptomes/
 
-for(( t=0; t<${#ARRAY[@]}; t++))                                                        # for loop that iterates through .fastq files
-do                                                                                      # begin ConDeTri & FastQC for loop
+# places paths to files ending in ".fastq" into an array
+ARRAY=(`find $home -name '*.fastq'`)
 
-        echo $t                                                                         # print current iteration
-        echo ${ARRAY[$t]}                                                               # print path t
-        echo ${ARRAY1[$t]}                                                              # print file t
-        x=${#ARRAY1[$t]}                                                                # length of the element located at index t
-        echo ${ARRAY1[$t]:0:$x-6}                                                       # removes last six characters (.fastq) from element located at index t
+# places files ending in ".fastq" into an array
+ARRAY1=(`find $home -name '*.fastq' -exec basename {} \;`)                  
 
+# for loop that iterates through ".fastq" files
+for(( t=0; t<${#ARRAY[@]}; t++))
+do
 
-        perl ~/ConDeTri/condetri_v2.2.pl -fastq1=${ARRAY[$t]} -prefix=${ARRAY1[$t]:0:$x-6} -hq=25 -lq=10 -frac=.8 -minlen=50 -mh=45 -ml=5 -sc=33        # run ConDeTri
-        mv ${ARRAY1[$t]:0:$x-6}* ~/transcriptomes/${ARRAY1[$t]:0:$x-6}                  # move ConDeTri outputs to corresponding directories ("_trim.fastq" and ".stats")
+        echo $home
+        echo $t                         # print current iteration
+        echo ${ARRAY[$t]}               # print path t
+        echo ${ARRAY1[$t]}              # print file t
+        # length of the element located at index t
+        x=${#ARRAY1[$t]}
+	# removes last six characters (.fastq) from element located at index t
+	BASE=${ARRAY1[$t]:0:$x-6}
+	echo $BASE		        # print file without ".fastq"
 
-        ./~/FastQC/fastqc -o=~/transcriptomes/${ARRAY1[$t]:0:$x-6}/ --noextract ~/transcriptomes/${ARRAY1[$t]:0:$x-6}/${ARRAY1[$t]:0:$x-6}_trim.fastq   # run FastQC
+	# run ConDeTri
+        perl ~/ConDeTri/condetri_v2.2.pl -fastq1=${ARRAY[$t]} -prefix=$BASE -hq=25 -lq=10 -frac=.8 -minlen=50 -mh=45 -ml=5 -sc=33
+        
+        # move ConDeTri outputs to corresponding directories ("_trim.fastq" and ".stats")
+        mv $BASE* $home$BASE
 
-done                                                                                    # end ConDeTri & FastQC for loop
+	# run FastQC
+        ~/FastQC/fastqc -o=$home$BASE --noextract $home$BASE/$BASE"_trim.fastq"
 
-ARRAY2=(`find ~/transcriptomes/ -name '*_trim.fastq'`)                                  # places paths to ConDeTri outputs into an array
+done                                                                                  
 
-for(( t=0; t<${#ARRAY2[@]}; t++))                                                       # for loop that iterates through ConDeTri files
-do                                                                                      # begin Trinity for loop
+# places paths to ConDeTri outputs into an array
+ARRAY2=(`find $home -name '*_trim.fastq'`)                                  
 
-        x=${#ARRAY2[$t]}                                                                # length of the element located at index t
-        mv ${ARRAY2[$t]} ${ARRAY2[$t]:0:$x-11}_m50_hq25_lq10_frac80_mh45_ml5.fq         # descriptively renames ConDeTri output
+# for loop that iterates through ConDeTri files
+for(( t=0; t<${#ARRAY2[@]}; t++))                                                       
+do                                                                                      
 
-        perl ~/../../c1/apps/trinity/r20131110/Trinity.pl --seqType fq --JM 10G  --single ${ARRAY2[$t]:0:$x-11}_m50_hq25_lq10_frac80_mh45_ml5.fq        # run Trinity
+	echo $t                         # print current iteration
+        echo ${ARRAY2[$t]}              # print path t
+	# length of the element located at index t
+        x=${#ARRAY2[$t]}
+        # removes last eleven characters (_trim.fastq) from element located at index t
+        path=${ARRAY2[$t]:0:$x-11}
+        echo $path  			# print path without "_trim.fastq"                          
+        
+        # descriptively renames ConDeTri output                                      
+        mv ${ARRAY2[$t]} $path"_m50_hq25_lq10_frac80_mh45_ml5_sc33.fq"         
 
-done                                                                                    # end Trinity for loop
+        # run Trinity
+        perl ~/../../c1/apps/trinity/r20131110/Trinity.pl --seqType fq --JM 10G  --single $path"_m50_hq25_lq10_frac80_mh45_ml5_sc33.fq"        
+
+done
